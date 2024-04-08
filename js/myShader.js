28d3cba1d8bbba71b6a1087595fb4eed35588b3c
Peter <e12119833@student.tuwien.ac.at>
2024-04-08 14:57:59 +0200

    Add uniforms to shader

--- js/myShader.js
+++ js/myShader.js
@@ -2,5 +2,8 @@ class TestShader extends Shader{
     constructor(material, cameraPos, boundDim){
         super("my_vert", "my_frag");
         // setUniform
+        this.setUniform("cameraPos", cameraPos);
+        this.setUniform("boundBox", boundDim);
+        this.setUniform("volume", material);
     }
 }
