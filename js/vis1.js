f1d57f40b7ebd1c56f45dfd836187dc035f6757f
Peter <e12119833@student.tuwien.ac.at>
2024-04-19 21:19:57 +0200

    work on histogram, finally get a reasonable volume rendering output

--- js/vis1.js
+++ js/vis1.js
@@ -78,9 +78,59 @@ async function resetVis(){
         texture3D.format = THREE.RedFormat;
         texture3D.type = THREE.FloatType;
         texture3D.minFilter = texture3D.magFilter = THREE.LinearFilter;
+        //texture3D.wrapS = texture3D.wrapT = texture3D.wrapR = THREE.RepeatWrapping;
         texture3D.unpackAligment = 1;
         texture3D.needsUpdate = true;
         myShader = new MyShader(texture3D, camera.position, boundDim, canvasWidth, canvasHeight);
+
+        let svg = d3.select("#histogram")
+            .append("svg")
+                .attr("width", 350)
+                .attr("height", 330)
+            .append("g")
+                .attr("transform", "translate(30,10)");
+
+        let x = d3.scaleLinear()
+            .domain([0.0, 1.0])
+            .range([0, 300]);
+        svg.append("g")
+            .attr("transform", "translate(0, 300)")
+            .call(d3.axisBottom(x));
+
+        let y = d3.scaleLinear()
+            .domain([1.0, 0.0])
+            .range([0, 300]);
+        svg.append("g")
+            .call(d3.axisLeft(y));
+
+        let binFunc = d3.histogram()
+            .domain(x.domain())
+            .thresholds(x.ticks(100));
+
+        let bins = binFunc(volume.voxels);
+        let binSizes = [];
+        
+        for (let i = 0; i != bins.length; i++) {
+            binSizes.push(bins[i].length / volume.voxels.length);
+        }
+
+        let minSize = Math.min.apply(Math, binSizes);
+        let maxSize = Math.max.apply(Math, binSizes);
+
+        let scaleFunction = function(n) { return (n - minSize) / (maxSize - minSize); };
+
+        console.log(scaleFunction(0.3));
+
+        svg.selectAll("rect")
+            .data(bins)
+            .enter()
+            .append("rect")
+                .attr("x", 2)
+                .attr("transform", function(d) { return "translate(" + x(d.x0) + ", " + y(scaleFunction(d.length / volume.voxels.length)) + ")"; })
+                .attr("width", function(d) { return x(d.x1) - x(d.x0) - 1; })
+                .attr("height", function(d) { return 300 - y(scaleFunction(d.length / volume.voxels.length)); })
+                .style("fill", "#cc2222");
+
     }
 
     // dummy scene: we render a box and attach our color test shader as material
